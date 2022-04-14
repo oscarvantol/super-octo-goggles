9a82f4263d7cacba23ca1db8239a87b5b9639358
Oscar van Tol <ovantol@gmail.com>
2022-04-14 22:57:41 +0200

    again

--- arrinstall.ps1
+++ arrinstall.ps1
@@ -1,5 +1,5 @@
 $url = "https://go.microsoft.com/fwlink/?LinkId=287166"
 $output = "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi"
 Invoke-WebRequest -Uri $url -OutFile $output
-./$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi /quiet
+& "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi" /quiet
 WebpiCmd.exe /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
