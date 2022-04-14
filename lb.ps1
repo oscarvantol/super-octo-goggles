618a134292dc1ed5a54adfbacf6098c14d975547
Oscar van Tol <ovantol@gmail.com>
2022-04-14 23:23:03 +0200

    sdf

--- lb.ps1
+++ lb.ps1
@@ -3,4 +3,4 @@ $url = "https://go.microsoft.com/fwlink/?LinkId=287166"
 $output = "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi"
 Invoke-WebRequest -Uri $url -OutFile $output
 & "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi" /quiet
-WebpiCmd.exe /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
+& "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd.exe" /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
