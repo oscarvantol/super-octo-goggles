8e95dbd4d707bd008869424f6269a8909aab55c7
Oscar van Tol <ovantol@gmail.com>
2022-04-14 17:03:21 +0200

    change root

--- arrinstall.ps1
+++ arrinstall.ps1
@@ -1,5 +1,6 @@
 $url = "https://go.microsoft.com/fwlink/?LinkId=287166"
 $output = "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi"
 Invoke-WebRequest -Uri $url -OutFile $output
+cd $PSScriptRoot
 .\WebPlatformInstaller_amd64_en-US.msi /quiet
 WebpiCmd.exe /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
