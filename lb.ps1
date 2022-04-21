aefb093949caef2db6acfb36a2f9992d7fe628b1
Oscar van Tol <ovantol@gmail.com>
2022-04-21 15:45:05 +0200

    Update lb.ps1
    t

--- lb.ps1
+++ lb.ps1
@@ -1,13 +1,14 @@
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
 $url = "https://go.microsoft.com/fwlink/?LinkId=287166"
 $output = "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi"
 Invoke-WebRequest -Uri $url -OutFile $output
 & "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi" /qn /norestart
-Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
 $count=0;
 $maxTries = 20
 do {
-    If (-not (Test-Path 'C:\Program Files\Microsoft\Web Platform Installert')) {
+    If (-not (Test-Path 'C:\Program Files\Microsoft\Web Platform Installer')) {
         Write-Output '.'
         Start-Sleep -Seconds 5
         $count++
@@ -22,4 +23,4 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 WebpiCmd.exe /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
 
 Install-WindowsFeature NET-WCF-HTTP-Activation45
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
