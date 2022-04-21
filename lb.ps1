1ed1cfbc22acd1c1c6c14ac09c61463e1b5677b8
Oscar van Tol <ovantol@gmail.com>
2022-04-21 10:12:29 +0200

    Update lb.ps1
    t

--- lb.ps1
+++ lb.ps1
@@ -2,6 +2,6 @@ Install-WindowsFeature -name Web-Server -IncludeManagementTools
 $url = "https://go.microsoft.com/fwlink/?LinkId=287166"
 $output = "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi"
 Invoke-WebRequest -Uri $url -OutFile $output
-& "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi" /quiet
-Set-Location 'C:\Program Files\Microsoft\Web Platform Installer'
-.\WebpiCmd.exe /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
+& "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi" /qn /norestart
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+WebpiCmd.exe /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
