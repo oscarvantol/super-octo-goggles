54f05405b01b664400721a788b2970dad18d1ea4
Oscar van Tol <ovantol@gmail.com>
2022-04-21 14:21:28 +0200

    Update lb.ps1

--- lb.ps1
+++ lb.ps1
@@ -1,7 +1,83 @@
-Install-WindowsFeature -name Web-Server -IncludeManagementTools
+#Install-WindowsFeature -name Web-Server -IncludeManagementTools
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Security
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-URLAuthorization
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-IPSecurity
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Performance
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionDynamic
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementScriptingTools
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-IIS6ManagementCompatibility
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-Metabase
+#Enable-WindowsOptionalFeature -Online -FeatureName WAS-WindowsActivationService
+#Enable-WindowsOptionalFeature -Online -FeatureName WAS-ProcessModel
+#Enable-WindowsOptionalFeature -Online -FeatureName WAS-ConfigurationAPI
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-HostableWebCore
+Enable-WindowsOptionalFeature -Online -FeatureName WCF-HTTP-Activation
+Enable-WindowsOptionalFeature -Online -FeatureName WCF-NonHTTP-Activation
+Enable-WindowsOptionalFeature -Online -FeatureName WCF-Services45
+Enable-WindowsOptionalFeature -Online -FeatureName WCF-HTTP-Activation45
+Enable-WindowsOptionalFeature -Online -FeatureName WCF-TCP-Activation45
+#Enable-WindowsOptionalFeature -Online -FeatureName WCF-Pipe-Activation45
+#Enable-WindowsOptionalFeature -Online -FeatureName WCF-MSMQ-Activation45
+#Enable-WindowsOptionalFeature -Online -FeatureName WCF-TCP-PortSharing45
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-CertProvider
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-DigestAuthentication
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ClientCertificateMappingAuthentication
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-IISCertificateMappingAuthentication
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ODBCLogging
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-StaticContent
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-DirectoryBrowsing
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebDAV
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASP
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-CGI
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ServerSideIncludes
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-CustomLogging
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementService
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-WMICompatibility
+
 $url = "https://go.microsoft.com/fwlink/?LinkId=287166"
 $output = "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi"
 Invoke-WebRequest -Uri $url -OutFile $output
 & "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi" /qn /norestart
+
+
+$count=0;
+$maxTries = 20
+do {
+    If (-not (Test-Path 'C:\Program Files\Microsoft\Web Platform Installert')) {
+        Write-Output 'waiting'
+        Start-Sleep -Seconds 5
+        $count++
+    } else {
+        $count = $maxTries
+    }
+
+} While ($count -lt $maxTries)
+
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 WebpiCmd.exe /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
+
