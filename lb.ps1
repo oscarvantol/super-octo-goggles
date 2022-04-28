060318fbdbead0c0b8f484152b3cb61ed89f628c
Oscar van Tol <ovantol@gmail.com>
2022-04-28 11:40:36 +0200

    test-direct-install

--- lb.ps1
+++ lb.ps1
@@ -1,15 +1,9 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
-$url = "https://go.microsoft.com/fwlink/?LinkId=287166"
-$output = "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi"
-Invoke-WebRequest -Uri $url -OutFile $output
-& "$PSScriptRoot\WebPlatformInstaller_amd64_en-US.msi" /qn /norestart -Out-Host
-
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
-WebpiCmd.exe /Install /Products:'UrlRewrite2,ARRv3_0' /AcceptEULA /Log:WebpiCmd.log
-
-Install-WindowsFeature NET-WCF-HTTP-Activation45
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
+$arrUrl = "https://download.microsoft.com/download/E/9/8/E9849D6A-020E-47E4-9FD0-A023E99B54EB/requestRouter_amd64.msi"
+$arrFile = "$PSScriptRoot\requestRouter_amd64.msi" 
+Invoke-WebRequest -Uri $arrUrl -OutFile $arrFile
+& $arrFile /qn /norestart -Out-Host
 
 $dotnet6download = "https://download.visualstudio.microsoft.com/download/pr/0c2039d2-0072-43a8-bb20-766b9a91d001/0e2288a2f07743e63778416b2367bb88/dotnet-hosting-6.0.4-win.exe"
 $dotnet6file = "$PSScriptRoot\dotnet-hosting-6.0.4-win.exe"
@@ -20,3 +14,6 @@ $dnframworkdownload = "https://go.microsoft.com/fwlink/?LinkId=2085155"
 $dnframworkfile = "$PSScriptRoot\ndp48-web.exe"
 Invoke-WebRequest -Uri $dnframworkdownload -OutFile $dnframworkfile
 & $dnframworkfile /q | Out-Host
+
+Install-WindowsFeature NET-WCF-HTTP-Activation45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
