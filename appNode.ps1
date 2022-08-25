3bcc908f14731e728d8469c9391a6e5f3cdfec58
Oscar van Tol <ovantol@gmail.com>
2022-08-25 15:41:52 +0200

    Update appNode.ps1

--- appNode.ps1
+++ appNode.ps1
@@ -1,5 +1,14 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionDynamic
+Enable-WindowsOptionalFeature -Online -FeatureName WCF-HTTP-Activation
+Enable-WindowsOptionalFeature -Online -FeatureName WCF-NonHTTP-Activation
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
 
+ 
 $dotnet6download = "https://download.visualstudio.microsoft.com/download/pr/0c2039d2-0072-43a8-bb20-766b9a91d001/0e2288a2f07743e63778416b2367bb88/dotnet-hosting-6.0.4-win.exe"
 $dotnet6file = "$PSScriptRoot\dotnet-hosting-6.0.4-win.exe"
 Invoke-WebRequest -Uri $dotnet6download -OutFile $dotnet6file
@@ -10,3 +19,5 @@ $dnframworkfile = "$PSScriptRoot\ndp48-web.exe"
 Invoke-WebRequest -Uri $dnframworkdownload -OutFile $dnframworkfile
 & $dnframworkfile /q | Out-Host
 
+Install-WindowsFeature NET-WCF-HTTP-Activation45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
