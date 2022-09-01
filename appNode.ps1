b673a68cea92bac2026425ac3fc187ea50b21b0d
Oscar van Tol <ovantol@gmail.com>
2022-09-01 12:22:10 +0200

    install rewrite module

--- appNode.ps1
+++ appNode.ps1
@@ -11,6 +11,8 @@ Enable-WindowsOptionalFeature -Online -FeatureName WCF-HTTP-Activation -All
 Enable-WindowsOptionalFeature -Online -FeatureName WCF-NonHTTP-Activation -All
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication -All
 
+$rwUrl = 'https://download.microsoft.com/download/1/2/8/128E2E22-C1B9-44A4-BE2A-5859ED1D4592/rewrite_amd64_en-US.msi'
+Start-Process msiexec.exe -Wait -ArgumentList "/I $rwUrl /quiet"
  
 #$dotnet6download = "https://download.visualstudio.microsoft.com/download/pr/0c2039d2-0072-43a8-bb20-766b9a91d001/0e2288a2f07743e63778416b2367bb88/dotnet-hosting-6.0.4-win.exe"
 #$dotnet6file = "$PSScriptRoot\dotnet-hosting-6.0.4-win.exe"
